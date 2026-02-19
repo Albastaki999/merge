a9dac013f8fc7ee82532b99b53db2b16798c318c
Huzaifa Jahid Jambagi <huzaifajambagi10@gmail.com>
2026-02-19 22:35:15 +0530

    idksjd

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function test() {
     let name = "something";
     let age = 10;
 
-    if (age > 18) {
+    if (age >= 18) {
         console.log("Eligible to vote");
     }
+    console.log("Name: " + name);   
 }
