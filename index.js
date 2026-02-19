ff01cba5c6c970222253e8dd8b419a664e641a5d
Rashid Mahmood <rashidmahmood06056@gmail.com>
2026-02-19 22:38:17 +0530

    push

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ function test() {
     if (age >= 18) {
         console.log("Eligible to vote");
     }
+
+    if (name.length() > 10) {
+        console.log("Name cannot contain more than 10 chars");
+    }
     console.log("Name: " + name);   
 }
